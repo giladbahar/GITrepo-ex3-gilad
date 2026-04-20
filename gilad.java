fa72845bce38867efb6f2aa16dfd4c0e40e1bc5c
giladbahar <giladb301@gmail.com>
2026-04-20 11:22:19 +0300

    update gilad print

--- gilad.java
+++ gilad.java
@@ -1,7 +1,7 @@
 public class gilad {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++) {
-            System.out.println("Gilad");
+            System.out.println("Gilad updated");
         }
     }
 }
